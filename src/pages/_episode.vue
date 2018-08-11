2733d816624113797eb72717c06d54c54e86e31e
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-08-11 18:35:14 -0300

    fisrt ep

--- src/pages/_episode.vue
+++ src/pages/_episode.vue
@@ -1,34 +1,20 @@
 <template>
-  <section class="content-container">
-    <h1 class="post-title"> {{ post.title }} </h1>
+  <section>
+    <h1 class="is-size-1"> {{ post.title }} </h1>
+    <Youtube :src="post.youtube" />
     <nuxtent-body :body="post.body" />
   </section>
 </template>
 
 <script>
+import Youtube from '../components/youtube.vue'
+
 export default {
+  components: { Youtube },
   asyncData: async ({ app, route, payload }) => {
-    const { section, slug } = route.params
-    console.log({ section, slug })
     return ({
       post: payload || await app.$content('/seasons').get(route.path)
     })
   }
 }
 </script>
-
-<style>
-.content-container
-{
-  width: 38rem;
-  margin: 0 auto
-}
-
-.post-title
-{
-  font-size: 2.5rem;
-  margin-bottom: 1rem;
-  font-weight: 500;
-  color: #35495e;
-}
-</style>
