2dbe005b5ec92b26e4598deb9acc63853ffa7882
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-08-11 18:35:04 -0300

    page header

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+}
+</script>
+
+<template>
+  <header
+    class="has-background-light	">
+    <nav
+      class="navbar container is-light"
+      role="navigation"
+      aria-label="main navigation">
+      <div class="navbar-brand">
+        Vue.js Brasil - Podcast
+      </div>
+    </nav>
+  </header>
+</template>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,16 @@
+<script>
+import PageHeader from '../components/PageHeader.vue'
+
+export default {
+  components: { PageHeader }
+}
+</script>
+
 <template>
   <div>
-    <nuxt/>
+    <PageHeader />
+    <div class="container">
+      <nuxt/>
+    </div>
   </div>
 </template>
